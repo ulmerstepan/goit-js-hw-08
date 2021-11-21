ed3a4b2a81e565fb19840f1990aee356ddca1b40
Stepan Ulmer <ulmer.st@gmail.com>
2021-11-21 19:05:32 +0200

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const imgsContainer = document.querySelector('.gallery');
+const imgsMarkup = createImgGallary(galleryItems);
+
+imgsContainer.insertAdjacentHTML('beforeend', imgsMarkup);
+
+function createImgGallary(gallaryData) {
+    return gallaryData.map(({ preview, original, description }) => {
+        return `
+            <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+            </a>
+        `;
+    })
+        .join('');
+
+};
+    
+var lightbox = new SimpleLightbox('.gallery a');
+lightbox.options.captionDelay = 250;
