1ef5a53389dd006d4b9fd0a0ee1351d1545444ea
Stepan Ulmer <ulmer.st@gmail.com>
2021-11-22 21:40:29 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash/throttle';
 
+const playerIframe = document.querySelector('#vimeo-player');
+const player = new Player(playerIframe);
+
+player.on('timeupdate', throttle(currentTime, 1000));
+function currentTime(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
